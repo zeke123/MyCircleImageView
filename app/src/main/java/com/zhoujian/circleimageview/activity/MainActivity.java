ebbeaf8583e64d24fd245047bd5b2bcb3a28b1f6
zhoujian <1064791247@qq.com>
2017-01-06 17:19:40 +0800

    zeke  圆形图片的使用

---
+++ app/src/main/java/com/zhoujian/circleimageview/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zhoujian.circleimageview.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.zhoujian.circleimageview.R;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+}
